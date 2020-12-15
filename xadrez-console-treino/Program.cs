179135feaa66d13f6447a7a0ceab7fb72ec0489f
Raphael Scursoni <ra.scursoni@gmail.com>
2020-12-15 14:44:51 -0300

    Implementando método para imprimir o tabuleiro

--- xadrez-console-treino/Program.cs
+++ xadrez-console-treino/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console_treino
 {
@@ -7,9 +7,8 @@ namespace xadrez_console_treino
     {
         static void Main(string[] args)
         {
-            Posicao p;
-            p = new Posicao(3, 4);
-            Console.WriteLine("Posição" + p);
-        }
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
+            Console.ReadLine();        }
     }
 }
