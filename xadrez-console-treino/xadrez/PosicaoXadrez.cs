ff7259578bfc056c58586f40ecaef07c698cb53d
Raphael Scursoni <ra.scursoni@gmail.com>
2020-12-16 10:19:00 -0300

    criando a classe PosicaoXadrez

---
+++ xadrez-console-treino/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace xadrez {
+    class PosicaoXadrez {
+        public char coluna { get; set; }
+        public char linha { get; set; }
+
+        public PosicaoXadrez(char coluna, char linha) {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public override string ToString() {
+            return "" + coluna + linha;
+        }
+    }
+}
