baf9695078c43e589322a4af2e6dd1940021a656
Raphael Scursoni <ra.scursoni@gmail.com>
2020-12-16 10:12:32 -0300

    Tratando erros com excessões personalizadas.

--- xadrez-console-treino/Tabuleiro/Tabuleiro.cs
+++ xadrez-console-treino/Tabuleiro/Tabuleiro.cs
@@ -9,24 +9,39 @@
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+
+        //leitura da peça na posição dela
         public Peca peca(int linha, int coluna) {
             return pecas[linha, coluna];
         }
+
+        //sobrecarga de leitura da peça na posição dela
         public Peca peca(Posicao pos) {
             return pecas[pos.Linha, pos.Coluna];
         }
-        public void colocarPeca(Peca p, Posicao pos) {
-            pecas[pos.Linha, pos.Coluna] = p;
-            p.posicao = pos;
-        }
 
+        //verifica se tem exeption, retorna se tem peça.
+        public bool existePeca(Posicao pos) {
+            validarPosicao(pos);
+            return peca(pos) is null;
+        }
+        public void colocarPeca(Peca p, Posicao pos) {
+            if (existePeca(pos)) {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
+            else {
+                pecas[pos.Linha, pos.Coluna] = p;
+                p.posicao = pos;
+            }
+        }
         public bool posicaoValida(Posicao pos) {
-            if(pos.Linha<0 || pos.Linha>=linhas || pos.Coluna<0 || pos.Coluna >= colunas) {
+            if (pos.Linha < 0 || pos.Linha >= linhas || pos.Coluna < 0 || pos.Coluna >= colunas) {
                 return false;
             }
             return true;
         }
 
+        //Método para retorno de exception personalizada
         public void validarPosicao(Posicao pos) {
             if (!posicaoValida(pos)) {
                 throw new TabuleiroException("Posição Inválida!");
