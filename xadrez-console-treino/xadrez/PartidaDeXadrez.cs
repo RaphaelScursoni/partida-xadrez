ca9d17dd5b67dc0413b079c9398495ee9c19001c
Raphael Scursoni <ra.scursoni@gmail.com>
2020-12-16 11:11:12 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console-treino/xadrez/PartidaDeXadrez.cs
+++ xadrez-console-treino/xadrez/PartidaDeXadrez.cs
@@ -4,9 +4,10 @@ using tabuleiro;
 namespace xadrez {
     class PartidaDeXadrez {
 
-        public Tabuleiro tab { get; set; }
+        public Tabuleiro tab { get; private set; }
         private int turno;
         private Cor jogadorAtual;
+        public bool terminada { get; private set; }
 
         public PartidaDeXadrez() {
             tab = new Tabuleiro(8, 8);
